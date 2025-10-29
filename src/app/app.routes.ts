f267f710f4a0efbce6a28c0609adfce75ee0dea1
Hendrik <hendrik.b.kaiber@gmail.com>
2025-10-29 19:41:35 -0300

    Adicionar componente para listar cliente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { CadastroClienteComponent } from './components/clientes/cadastro-cliente/cadastro-cliente.component';
 import { CadastroServicoComponent } from './components/servicos/cadastro-servico/cadastro-servico.component';
 import { ListarAnimalComponent } from './components/animais/listar-animal/listar-animal.component';
+import { ListarClienteComponent } from './components/clientes/listar-cliente/listar-cliente.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -11,4 +12,5 @@ export const routes: Routes = [
     { path: 'clientes/cadastro-cliente', component: CadastroClienteComponent },
     { path: 'servicos/cadastro-servico', component: CadastroServicoComponent },
     { path: 'animais/listar-animais', component: ListarAnimalComponent },
+    { path: 'clientes/listar-clientes', component: ListarClienteComponent },
 ];
--- src/app/components/clientes/listar-cliente/listar-cliente.component.ts
+++ src/app/components/clientes/listar-cliente/listar-cliente.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../../../models/cliente.model';
+import { ClienteService } from '../../../services/clientes.service';
 
 @Component({
   selector: 'app-listar-cliente',
@@ -7,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './listar-cliente.component.css'
 })
 export class ListarClienteComponent {
-
+  clientes: Cliente[] = [];
+  constructor(private clienteservice: ClienteService) { }
+  ngOnInit() {
+    this.getAllClientes();
+  }
+  getAllClientes() {
+    this.clienteservice.getAllClientes().then((clientes: Cliente[]) => {
+      this.clientes = clientes;
+    });
+  }
 }
